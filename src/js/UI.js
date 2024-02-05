14030686e253899792de9053b21391ce1af9431b
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2024-02-05 14:38:12 -0600

    fixed submit button not being assigned correctly

--- src/js/UI.js
+++ src/js/UI.js
@@ -6,14 +6,19 @@ const main = document.querySelector("main");
 class UI {
   static loadHome() {
     this.loadTasks();
+    this.loadProjects();
     this.loadEventListeners();
   }
 
   static loadTasks() {
-    //main.innerHTML = "<h1>Hello World</h1>";
+    // this.clearMain();
+    // main.innerHTML = "<h1>Tasks</h1>";
   }
 
-  static loadProjects() {}
+  static loadProjects() {
+    // this.clearMain();
+    // main.innerHTML = `<h1>${project.name}</h1>`;
+  }
 
   static loadEventListeners() {
     this.createTask();
@@ -21,43 +26,56 @@ class UI {
 
   static createTask() {
     const addTaskBtn = document.querySelector("#add-task");
-    let newTaskWindow = 0;
+    let windowOpen = false;
     addTaskBtn.addEventListener("click", addTaskDisplay);
 
     function addTaskDisplay() {
-      if (newTaskWindow === 0) {
-        newTaskWindow++;
+      if (windowOpen === false) {
+        windowOpen = true;
+
         const newTaskCtn = document.createElement("div");
         newTaskCtn.classList.add("new-task-bg");
+
         const newTaskForm = document.createElement("form");
         newTaskForm.classList.add("new-task-form");
+
         newTaskForm.innerHTML = `<div class="input-ctn" id="title-ctn">
-        <label>Title</label>
-        <input type="text" id="title" required />
-      </div>
-      <div class="input-ctn" id="description-ctn">
-        <label>Description</label>
-        <input />
-      </div>
-      <div class="input-ctn" id="due-date-ctn">
-        <label>Due Date</label>
-        <input type="date" required />
-      </div>
-      <div class="input-ctn" id="project-ctn">
-        <label>Project</label>
-        <input type="text" />
-      </div>
-      <button type="submit">Create Task</button>`;
+        <label for="title">Title</label>
+        <input type="text" id="title" />
+        </div>
+        <div class="input-ctn" id="description-ctn">
+          <label for="description">Description</label>
+          <input type="text" id="description"/>
+        </div>
+        <div class="input-ctn" id="due-date-ctn">
+          <label for="due-date">Due Date</label>
+          <input id="due-date" type="date" />
+        </div>
+        <div class="input-ctn" id="project-ctn">
+          <label for="project">Project</label>
+          <input id="project" type="text" />
+        </div>
+        <button id="submit-new-task" type="submit">Create Task</button>`;
+
         newTaskCtn.addEventListener("click", (e) => {
           if (e.target === newTaskCtn) {
             main.removeChild(newTaskCtn);
-            newTaskWindow--;
+            windowOpen = false;
           }
         });
 
         newTaskCtn.appendChild(newTaskForm);
         main.prepend(newTaskCtn);
+
+        let submitTaskBtn = document.querySelector("#submit-new-task");
+        submitTaskBtn.addEventListener("click", (e) => {
+          e.preventDefault();
+        });
       }
     }
+  }
+
+  static clearMain() {
+    main.innerHTML = "";
   }
 }
